468b85ef12aec6d04f0bb6ae38252f68345a558d
St0new4ll <nstone@unomaha.edu>
2022-11-18 12:51:08 -0600

    Pushing account

---
+++ account.py
@@ -0,0 +1,26 @@
+class Account:
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount: float):
+        if amount <= 0:
+            return False
+        else:
+            self.__account_balance += amount
+            return True
+
+    def withdraw(self, amount: float):
+        if amount > self.__account_balance:
+            return False
+        elif amount <= 0:
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+
+    def get_balance(self):
+        print(self.__account_balance)
+
+    def get_name(self):
+        print(self.__account_name)
